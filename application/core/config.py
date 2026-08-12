6bd4ffb5ab75896693e13aaac12ebfb33c0a7800
Tim1ro <artem.gr.2006@gmail.com>
2026-08-12 18:33:57 +0300

    create settings_config

---
+++ application/core/config.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = '0.0.0.0'
+    port: int = 8000
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = '/v1'
+    users: str = '/users'
+
+class ApiPrefix(BaseModel):
+    prefix: str = '/api'
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='APP_CONFIG__',
+    )
+    run: RunConfig = RunConfig()
+    db: DatabaseConfig = DatabaseConfig()
+    api: ApiPrefix = ApiPrefix()
+
+settings = Settings()
