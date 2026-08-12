e0e2d03dc86e86e70e2654ddd932274b4afd2664
Tim1ro <artem.gr.2006@gmail.com>
2026-08-12 19:30:36 +0300

    create User table

---
+++ application/core/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+from .base import Base
+
+if TYPE_CHECKING:
+    from .post import Post
+class User(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(default=True, server_default='true')
+
+    posts: Mapped[list['Post']] = relationship(back_populates='author')
